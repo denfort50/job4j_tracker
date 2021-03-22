1db20900440566aa587e84ba7af4a6a8536f4ab7
denfort50 <denfort50@yandex.ru>
2021-03-22 22:27:28 +0300

    2. Модель данных. [#467975]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,12 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Kalchenko Denis Alexandrovich");
+        student.setGroup("E-531");
+        student.setEntered("01.09.2017");
+        System.out.println(student.getFio() + System.lineSeparator() + "Group: " + student.getGroup()
+                + System.lineSeparator() + "Date: " + student.getEntered());
+    }
+}
