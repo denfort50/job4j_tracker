e9977e6f2c552b8f52dcdd59d63dd4022519ef7c
denfort50 <denfort50@yandex.ru>
2021-03-27 09:02:13 +0300

    7. Метод удаления заявки Tracker.delete [#467980]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,10 +56,12 @@ public class Tracker {
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        items[index] = null;
-        System.arraycopy(items, index + 1, items, index, size - index - 1);
-        items[size - 1] = null;
-        size--;
+        if (index != -1) {
+            items[index] = null;
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
         return index != -1;
     }
 }
