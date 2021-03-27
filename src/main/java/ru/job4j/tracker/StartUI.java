f6af2bbe7aa54cfc6a0aec67d68618e053177b32
denfort50 <denfort50@yandex.ru>
2021-03-27 10:11:05 +0300

    9. toString [#467983]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,11 +2,9 @@ package ru.job4j.tracker;
 
 public class StartUI {
     public static void main(String[] args) {
-        Tracker tracker = new Tracker();
         Item item = new Item();
         item.setName("Application");
-        tracker.add(item);
-        String result = tracker.findById(1).getName();
-        System.out.println(result);
+        item.setId(1);
+        System.out.println(item.toString());
     }
 }
