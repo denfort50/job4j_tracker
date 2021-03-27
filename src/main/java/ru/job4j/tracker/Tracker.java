c5bbf8869c9d8af01bdc6f3bb1e73cdec6feb1db
denfort50 <denfort50@yandex.ru>
2021-03-27 09:00:06 +0300

    6. Метод замены заявки. Tracker.replace [#467979]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,8 +47,10 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        item.setId(id);
-        items[index] = item;
+        if (index != -1) {
+            item.setId(id);
+            items[index] = item;
+        }
         return index != -1;
     }
 
