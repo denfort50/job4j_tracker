d8967ccb36a71a694bcf3d251f0458d782a54f6b
denfort50 <denfort50@yandex.ru>
2021-04-27 08:34:26 +0300

    0. Map, HashMap [#467891]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("defort50@yandex.ru", "Kalchenko Denis Alexandrovich");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
