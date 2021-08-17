2189cc166212dc426346bcbfa7fe56f48630e3b5
denfort50 <denfort50@yandex.ru>
2021-08-17 22:31:39 +0300

    2. Список адресов [#110062]

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class ProfilesTest {
+
+    @Test
+    public void whenCorrect() {
+        List<Profile> profiles = Arrays.asList(
+                new Profile(new Address("Москва", "Ленинский проспект", 11, 34)),
+                new Profile(new Address("Санкт-Петербург", "Сталинский проспект", 5, 10)),
+                new Profile(new Address("Тольятти", "Ясный проспект", 9, 20)));
+        List<Address> expect = Arrays.asList(
+                new Address("Москва", "Ленинский проспект", 11, 34),
+                new Address("Санкт-Петербург", "Сталинский проспект", 5, 10),
+                new Address("Тольятти", "Ясный проспект", 9, 20));
+        List<Address> result = Profiles.collect(profiles);
+        assertThat(result, is(expect));
+    }
+
+}
